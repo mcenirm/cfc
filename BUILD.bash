c7a90548ef8428be1ba38568ed6a2451388294db
Michael McEniry (UAH) <mcenirm@uah.edu>
2014-11-21 23:04:35 -0600

    (WIP) hdf5 and netcdf4

--- BUILD.bash
+++ BUILD.bash
@@ -2,14 +2,49 @@
 # Build CFchecker (https://bitbucket.org/mde_/cfchecker)
 # and any dependencies.
 
-declare -r top_dir=$(readlink -f $(dirname "$0"))
-declare -r dist_dir=${top_dir}/dist
-declare -r src_dir=${top_dir}/src
+declare -r prefix=$(readlink -f $(dirname "$0"))
+declare -r dist_dir=${prefix}/dist
+declare -r src_dir=${prefix}/src
+
+export LD_LIBRARY_PATH=${prefix}/lib${LD_LIBRARY_PATH+:${LD_LIBRARY_PATH}}
 
 mkdir -p -- "${src_dir}" || exit $?
-cd -- "${src_dir}" || exit $?
 
-tar xf "${dist_dir}/CFchecker-1.6.0.tar.bz2" || exit $?
-cd -- CFchecker || exit $?
-python setup.py
+# HDF5
+if ! [[ -e "${prefix}/bin/h5dump" ]] ; then
+  hdf5=hdf5-1.8.14-linux-centos6-x86_64-gcc447-shared
+  cd -- "${src_dir}" || exit $?
+  if [[ -d "${hdf5}" ]] ; then
+    rm -r "${hdf5}" || exit $?
+  fi
+  tar xf "${dist_dir}/${hdf5}.tar.gz" || exit $?
+  rsync -a "${hdf5}"/{bin,include,lib} "${prefix}/"
+  cd -- "${prefix}/bin" || exit $?
+  echo 'yes' | ./h5redeploy || exit $?
+fi
+
+# netcdf4
+if ! [[ -e "${prefix}/bin/ncdump" ]] ; then
+  nc4=netcdf-4.3.2
+  cd -- "${src_dir}" || exit $?
+  if [[ -d "${nc4}" ]] ; then
+    rm -r "${nc4}" || exit $?
+  fi
+  tar xf "${dist_dir}/${nc4}.tar.gz" || exit $?
+  cd -- "${nc4}" || exit $?
+  CPPFLAGS=-I${prefix}/include LDFLAGS=-L${prefix}/lib ./configure --prefix=${prefix}
+  make check install
+fi
+
+# netcdf4-python
+#if ! [[ -e "${prefix}/bin/h5dump" ]] ; then
+  nc4py=netcdf4-python-1.1.1rel
+  cd -- "${src_dir}" || exit $?
+  if [[ -d "${nc4py}" ]] ; then
+    rm -r "${nc4py}" || exit $?
+  fi
+  tar xf "${dist_dir}/${nc4py}.tar.gz" || exit $?
+  cd -- "${nc4py}" || exit $?
+  HDF5_DIR=${prefix} python setup.py
+#fi
 
--- DOWNLOAD.bash
+++ DOWNLOAD.bash
@@ -9,9 +9,9 @@ cd -- "${dist_dir}" || exit $?
 function dl () {
   md5=$1
   url=$2
-  file=${2##*/}
+  file=${3:-${2##*/}}
   if ! echo "${md5}  ${file}" | md5sum -c - >/dev/null 2>/dev/null ; then
-    curl -RO "${url}" || exit $?
+    curl -R -L -o "${file}" "${url}" || exit $?
     md5sum -- "${file}"
     echo Update "$0" with new md5
     exit 1
@@ -20,4 +20,7 @@ function dl () {
 
 # CFchecker
 dl 12a1da84d9f2403510fa3c2aeb1278f1 'http://redmine.iek.fz-juelich.de/attachments/download/209/CFchecker-1.6.0.tar.bz2'
+dl f93d2d41ad98a50ca4bfe9799ed807a0 'https://github.com/Unidata/netcdf4-python/archive/v1.1.1rel.tar.gz' 'netcdf4-python-1.1.1rel.tar.gz'
+dl 39b29e3580c0604f7cafc44a00d2ec4b 'http://www.hdfgroup.org/ftp/HDF5/current/bin/linux-centos6-x86_64-gcc447/hdf5-1.8.14-linux-centos6-x86_64-gcc447-shared.tar.gz'
+dl 2fd2365e1fe9685368cd6ab0ada532a0 'ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.2.tar.gz'
 
